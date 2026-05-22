d193df16135027e778824daa23bf2d62e67f145c
profecoellohn <profecoellohn@gmail.com>
2026-05-21 21:39:23 -0600

    1

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'sdc-erp-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './assets/css/styles.css',
+  './assets/js/app.js',
+  './assets/js/firebase.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => caches.match('./index.html')))
+  );
+});
