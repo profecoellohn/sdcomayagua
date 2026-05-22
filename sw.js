fda17e19456bd8196526d82acc302efcae75811a
profecoellohn <profecoellohn@gmail.com>
2026-05-21 21:30:04 -0600

    D

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[SDC Service Worker] Instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request));
-});
